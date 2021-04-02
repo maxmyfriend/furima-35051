d21ba6ea2adcab4829fc42d36c73cd92183346b7
maxmyfriend <80321521+maxmyfriend@users.noreply.github.com>
2021-04-02 15:57:27 +0900

    バリデーションを詳細に更新

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,19 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :kana_last_name, presence: true
-  validates :kana_first_name, presence: true
-  validates :birthday, presence: true
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
+  with_options presence: true do 
+    validates :nickname
+    validates :birthday
+
+    with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }  do 
+      validates :last_name
+      validates :first_name
+    end
+
+    with_options format: { with: /\A[ァ-ヶー－]+\z/ } do 
+      validates :kana_last_name
+      validates :kana_first_name
+    end
+  end
 end
