9609b7b58b80ee5eebde0d94a3e0b1c0911e9948
maxmyfriend <80321521+maxmyfriend@users.noreply.github.com>
2021-04-05 19:00:01 +0900

    ActiveHashの導入

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,26 @@
 class ProductsController < ApplicationController
   
   def index
+    @products = Product.order("created_at DESC")
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to root_path
+    else  
+      render :new 
+    end
   end
 
 
-private
+  private
 
-def product_params
-  params.require(:product),permit(:image, :name, :content, :category_id, :status_id, :delivery_fee_id, :prefecture_id, :arrival_id, :price).merge(user_id: current_user.id)
+  def product_params
+    params.require(:product).permit(:image, :name, :content, :category_id, :status_id, :delivery_fee_id, :prefecture_id, :arrival_date_id, :price).merge(user_id: current_user.id)
+  end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,11 @@
 class Product < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category, :status, :delivery_fee, :prefecture, :arrival_date 
+
+  validates :name, :content, :price, presence: true
+  validates :category_id, :status_id, :delivery_fee_id, :prefecture_id, :arrival_date_id, numericality: { other_than: 1 }
+
+  validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 300, leth_than_or_equal_to: 9999999}
 end
