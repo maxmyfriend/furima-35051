7620f8bd5e6dd11290507f0102f57d2751be5dc3
maxmyfriend <80321521+maxmyfriend@users.noreply.github.com>
2021-04-05 12:30:47 +0900

    productsテーブルの作成

---
+++ db/migrate/20210405031621_create_products.rb
@@ -0,0 +1,16 @@
+class CreateProducts < ActiveRecord::Migration[6.0]
+  def change
+    create_table :products do |t|
+      t.string     :name,            null: false
+      t.text       :content,         null: false
+      t.integer    :price,           null: false
+      t.integer    :category_id,     null: false
+      t.integer    :status_id,        null: false
+      t.integer    :delivery_fee_id, null: false
+      t.integer    :prefecture_id,   null: false
+      t.integer    :arrival_date_id, null: false
+      t.references :user,            null: false, foreign_key: true
+      t.timestamps
+    end
+  end
+end
