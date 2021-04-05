29f2807dac09e160f0ee95dc7235cbfca238fbbc
maxmyfriend <80321521+maxmyfriend@users.noreply.github.com>
2021-04-05 12:50:34 +0900

    Active Storageの導入

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,4 +3,9 @@ class ProductsController < ApplicationController
   def index
   end
 
+
+private
+
+def product_params
+  params.require(:product),permit(:image, :name, :content, :category_id, :status_id, :delivery_fee_id, :prefecture_id, :arrival_id, :price).merge(user_id: current_user.id)
 end
