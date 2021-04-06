2f2c864c9b91038e31db579ca88a38352fd568fe
maxmyfriend <80321521+maxmyfriend@users.noreply.github.com>
2021-04-06 12:25:28 +0900

    productsモデルの単体テストコードを記述

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,10 +2,15 @@ class Product < ApplicationRecord
   belongs_to :user
   has_one_attached :image
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :category, :status, :delivery_fee, :prefecture, :arrival_date 
+  belongs_to :category
+  belongs_to :status
+  belongs_to :delivery_fee
+  belongs_to :prefecture
+  belongs_to :arrival_date 
 
-  validates :name, :content, :price, presence: true
-  validates :category_id, :status_id, :delivery_fee_id, :prefecture_id, :arrival_date_id, numericality: { other_than: 1 }
+  validates :name, :content, :image, :price, presence: true
+  validates :category_id, :status_id, :delivery_fee_id, :prefecture_id, :arrival_date_id, numericality: { other_than: 1, message: "must be selected" }
 
-  validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 300, leth_than_or_equal_to: 9999999}
+  validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 300,less_than_or_equal_to: 9999999, message: "is from 300 to 9,999,999"}
+
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,73 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @product = FactoryBot.build(:product)
+  end
+
+  describe '商品の出品' do
+    context '商品の出品ができる場合' do
+      it "全ての情報が入力されていれば商品を出品できる" do
+        expect(@product).to be_valid
+      end
+    end
+
+    context '商品の出品ができない場合' do
+      it "商品画像が空だと出品できない" do
+        @product.image = nil
+        @product.valid?
+        expect(@product.errors.full_messages).to include("Image can't be blank")
+      end
+      it "商品名が空だと出品できない" do
+        @product.name = ''
+        @product.valid?
+        expect(@product.errors.full_messages).to include("Name can't be blank")
+      end
+      it "商品の説明が空だと出品できない" do
+        @product.content = ''
+        @product.valid?
+        expect(@product.errors.full_messages).to include("Content can't be blank")
+      end
+      it "商品のカテゴリーが---だと出品できない" do
+        @product.category_id = 1
+        @product.valid?
+        expect(@product.errors.full_messages).to include("Category must be selected")
+      end
+      it "商品の状態が---だと出品できない" do
+        @product.status_id = 1
+        @product.valid?
+        expect(@product.errors.full_messages).to include("Status must be selected")
+      end
+      it "配送料の負担が---だと出品できない" do
+        @product.delivery_fee_id = 1
+        @product.valid?
+        expect(@product.errors.full_messages).to include("Delivery fee must be selected")
+      end
+      it "配送元の地域が---だと出品できない" do
+        @product.prefecture_id = 1
+        @product.valid?
+        expect(@product.errors.full_messages).to include("Prefecture must be selected")
+      end
+      it "発送までの日数が---だと出品できない" do
+        @product.arrival_date_id = 1
+        @product.valid?
+        expect(@product.errors.full_messages).to include("Arrival date must be selected")
+      end
+      it "商品の販売価格が300円未満だと出品できない" do
+        @product.price = 299
+        @product.valid?
+        expect(@product.errors.full_messages).to include("Price is from 300 to 9,999,999")
+      end
+      it "商品の販売価格が9,999,999円を超えると出品できない" do
+        @product.price = 10000000
+        @product.valid?
+        expect(@product.errors.full_messages).to include("Price is from 300 to 9,999,999")
+      end
+      it "紐づくユーザーが存在しないと保存できないこと" do
+        @product.user = nil
+        @product.valid?
+        expect(@product.errors.full_messages).to include("User must exist")
+      end
+    end
+  end
 end
