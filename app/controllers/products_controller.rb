656a9945ebb1ecf9512cbfc891939e6a8a4e4b17
maxmyfriend <80321521+maxmyfriend@users.noreply.github.com>
2021-04-07 18:07:20 +0900

    同じ記述をまとめて可読性を上げた

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,7 @@
 class ProductsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_product, only: [:edit, :update, :show]
-  before_action :move_to_index, only: [:edit, :update]
+  before_action :set_product, only: [:edit, :update, :show, :destroy]
+  before_action :move_to_index, only: [:edit, :update, :destroy]
   
   def index
     @products = Product.order('created_at DESC')
@@ -35,8 +35,7 @@ class ProductsController < ApplicationController
   end
 
   def destroy
-    product = Product.find(params[:id])
-    product.destroy
+    @product.destroy
     redirect_to root_path
   end
 
