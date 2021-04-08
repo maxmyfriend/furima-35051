826557da3a6670dd2aa6138b2d09e0788e3f82eb
maxmyfriend <80321521+maxmyfriend@users.noreply.github.com>
2021-04-08 22:15:36 +0900

    Formオブジェクトの中身を更新

--- app/models/record_form.rb
+++ app/models/record_form.rb
@@ -1,11 +1,16 @@
 class RecordForm
+  
   include ActiveModel::Model
+  attr_accessor :user_id, :product_id, :prefecture_id, :post_cord, :city, :house_number, :building_name, :phone_number
 
-  attr_accessor :user, :product, :address
-  validates :user,   presence: true, foreign_key: true
-  validates :product, presence: true, foreign_key: true
-  validates :genre,   presence: true
-
+  with_options presece: true do
+    validates :user_id, :product_id, :city, :house_number
+    validates :post_cord, format: { with: /\A\d{3}[-]\d{4}\z/ }
+    validates :phone_number, format: { with: /0\d{9,10}/ }
+  end
+  
+  validates :prefecture_id, numericality: { other_than: 0, message: 'must be selected' }
+  
   def save
     Record.create(user: user, product: product)
     Address.create(address: address)
