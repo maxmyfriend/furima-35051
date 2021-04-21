1edfca968ebe088840fab380d15d8017fa215aa9
maxmyfriend <80321521+maxmyfriend@users.noreply.github.com>
2021-04-21 20:00:26 +0900

    各種モデルの日本語対応を調整

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,19 +5,23 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :products 
   has_many :records
-  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: 'を正しく入力してください' }, allow_blank: true
   with_options presence: true do 
     validates :nickname
     validates :birthday
+    validates :last_name
+    validates :first_name
+    validates :kana_last_name
+    validates :kana_first_name
+  end
 
-    with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }  do 
-      validates :last_name
-      validates :first_name
-    end
+  with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'を正しく入力してください' }, allow_blank: true do 
+    validates :last_name
+    validates :first_name
+  end
 
-    with_options format: { with: /\A[ァ-ヶー－]+\z/ } do 
-      validates :kana_last_name
-      validates :kana_first_name
-    end
+  with_options format: { with: /\A[ァ-ヶー－]+\z/, message: 'を正しく入力してください' }, allow_blank: true do 
+    validates :kana_last_name
+    validates :kana_first_name
   end
 end
