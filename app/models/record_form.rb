7d558fb3683a80da9aa24d2894e84e9cbf44396a
maxmyfriend <80321521+maxmyfriend@users.noreply.github.com>
2021-04-23 17:06:31 +0900

    商品購入時のエラーメッセージを日本語化

--- app/models/record_form.rb
+++ app/models/record_form.rb
@@ -3,16 +3,29 @@ class RecordForm
   attr_accessor :user_id, :product_id, :prefecture_id, :post_cord, :city, :house_number, :building_name, :phone_number, :token
 
   with_options presence: true do
-    validates :user_id, :product_id, :city, :house_number, :token
-    validates :post_cord, format: { with: /\A\d{3}-\d{4}\z/ }
-    validates :phone_number, format: { with: /0\d{9,10}\z/ }
+    validates :user_id
+    validates :product_id
+    validates :city
+    validates :house_number
+    validates :credit_message
+    validates :post_cord
+    validates :phone_number
   end
 
-  validates :prefecture_id, numericality: { other_than: 1, message: 'must be selected' }
+  validates :post_cord, format: { with: /\A\d{3}-\d{4}\z/ }, allow_blank: true
+  validates :phone_number, format: { with: /0\d{9,10}\z/ }, allow_blank: true
+  validates :prefecture_id, numericality: { other_than: 1, message: 'を選択してください' }
 
   def save
     record = Record.create(user_id: user_id, product_id: product_id)
     Address.create(prefecture_id: prefecture_id, post_cord: post_cord, city: city, house_number: house_number,
                    building_name: building_name, phone_number: phone_number, record_id: record.id)
+  end
+
+  private
+
+  def credit_message
+    return if token.present?
+    errors.add(:base, 'クレジットカード情報を入力してください')
   end
 end
